5e30f35469cbbd27ea30ad00466529f081f3cb65
Abhiiyer02 <93037572+Abhiiyer02@users.noreply.github.com>
2023-02-06 00:34:39 +0530

    Update 9red.cpp

--- 9red.cpp
+++ 9red.cpp
@@ -1,45 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <time.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int QUEUE_SIZE = 10;
-const int MAX_PACKETS = 20;
-const double MAX_PROBABILITY = 0.7;
-const double MIN_PROBABILITY = 0.3;
+int main(){
+    srand(time(0));
+    double qSize,qLimit,drop,curr_size=0,s=0;
+    int nop,min,max,r;
+    float avg;
+    cout << "Enter q size :" << endl;
+    cin >> qSize;
+    qLimit = 0.65*qSize;
+    cout << "Queue Threshold Set to : " << qLimit << endl;
+    cout << "Enter number of Packets: " << endl;
+    cin >> nop;
 
-// Generates a random number between min and max
-double rand_double(double min, double max) {
-  return min + (max - min) * (double) rand() / RAND_MAX;
+    for(int i=1;i<=nop ;i++){
+        if (curr_size >= qLimit){
+            cout << "Congestion detected !!" << endl; 
+            min = 0.1*curr_size ;
+            max = 0.25*curr_size;
+            int r = rand();
+            drop = min+(r%max);
+            cout << "R : " << r << " min : " << min << " max : " << max << endl;
+            cout << "Number of Packets dropped: " << drop << endl;
+            curr_size-=drop;
+        }
+        else 
+        {
+            cout << "Packet " << i << " Added to queue" << endl;
+            curr_size++;
+        }
+        cout << "Queue size " << curr_size << endl;
+        cout << endl;
+        sleep(1);
+    }
+
 }
-
-int main() {
-	// Seed the random number generator
-	srand(time(0));
-
-	int queue_size = 0;
-	double drop_probability = MIN_PROBABILITY;
-	for (int i = 0; i < MAX_PACKETS; i++) {
-		if (queue_size == QUEUE_SIZE) {
-			// Queue is full, drop the packet
-			cout << "Packet dropped (queue full)\n";
-			drop_probability = MIN_PROBABILITY;
-			cout<<"Drop probability is "<<drop_probability<<endl;
-			cout<<"Queue size is "<<queue_size<<endl;
-		} else if (rand_double(0, 1) < drop_probability) {
-			// Randomly drop the packet based on the current drop probability
-			cout << "Packet dropped (random)\n";
-			drop_probability += (MAX_PROBABILITY - MIN_PROBABILITY) / (MAX_PACKETS - 1);
-			cout<<"Drop probability is "<<drop_probability<<endl;
-			cout<<"Queue size is"<<queue_size<<endl;
-		} else {
-			// Accept the packet
-			cout << "Packet accepted\n";
-			queue_size++;
-			drop_probability = MIN_PROBABILITY;
-			cout<<"Drop probability is "<<drop_probability<<endl;
-			cout<<"Queue size is "<<queue_size<<endl;
-		}
-	}
-  	return 0;
-  }
